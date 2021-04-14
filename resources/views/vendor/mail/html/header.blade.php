0e377824ce34f950eecd9c5cce393898ba39dfa1
Michael Joyner <joyner.michael@gmail.com>
2021-04-14 11:00:14 +0800

    wip

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 {{--@if (trim($slot) === 'Laravel')--}}
-<img src="{{ url('/images/full_logo_navy.svg') }}" width="100" height="25.1" title="ELT Search Logo" class="logo" alt="ELT Search Logo">
+<img src="{{ url('/images/email_header_logo.png') }}" width="100" height="25.1" title="ELT Search Logo" class="logo" alt="ELT Search Logo">
 {{--@else--}}
 {{--{{ $slot }}--}}
 {{--@endif--}}
