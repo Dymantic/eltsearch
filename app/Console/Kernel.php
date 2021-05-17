2f78716189aa6440d10326c1e202c68e7b29c3ba
Michael Joyner <joyner.michael@gmail.com>
2021-05-17 09:29:39 +0800

    scheduling stuff

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('backup:clean')->dailyAt('01:00');
+         $schedule->command('backup:run')->dailyAt('01:30');
     }
 
     /**
