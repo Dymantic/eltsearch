a2c1fc1410d60c2af7f5731a90fb38e600ac86f3
Michael Joyner <joyner.michael@gmail.com>
2021-05-19 09:05:47 +0800

    handle empty posts on fe

--- resources/views/front/home/page.blade.php
+++ resources/views/front/home/page.blade.php
@@ -5,12 +5,17 @@
     @include('front.home.banner')
     <div class="py-20 px-6">
         <p class="type-h2 text-center text-navy">Latest Job Posts</p>
+        @if($posts->count() === 0)
+            <p class="type-b1 text-gray-600 max-w-2xl text-center mx-auto mt-12">Hi there. We haven't started posting any jobs yet, but be sure to check again in the future.</p>
+        @endif
         @foreach($posts as $post)
             @include('front.job-posts.index-card', ['post' => $post])
         @endforeach
     </div>
+    @if($posts->count() > 0)
     <div class="text-center my-12">
         <a href="/job-posts"
            class="btn-text">See more job posts &gt;</a>
     </div>
+    @endif
 </x-public-page>
--- resources/views/front/job-posts/index.blade.php
+++ resources/views/front/job-posts/index.blade.php
@@ -4,6 +4,10 @@
         <p class="max-w-xl mx-auto type-b1">Feel free to browse through the latest jobs here. Remeber, if you are a member, we find the jobs for you, and send it straight to your inbox. No need to be trawling posts like it's the dark ages.</p>
     </div>
 
+    @if($posts->count() === 0)
+        <p class="type-b1 text-gray-600 max-w-2xl text-center mx-auto mt-12">Hi there. We haven't started posting any jobs yet, but be sure to check again in the future.</p>
+    @endif
+
     <div class="pb-20 px-6">
         @foreach($posts as $post)
             @include('front.job-posts.index-card', ['post' => $post])
