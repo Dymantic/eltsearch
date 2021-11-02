7ed9b4cb10b90584adeb0c5006d07cd70fd18ebb
Michael Joyner <joyner.michael@gmail.com>
2021-11-02 10:41:12 +0800

    update copy

--- resources/views/front/home/page.blade.php
+++ resources/views/front/home/page.blade.php
@@ -6,7 +6,7 @@
     <div class="py-20 px-6">
         <p class="type-h2 text-center text-navy">Latest Job Posts</p>
         @if($posts->count() === 0)
-            <p class="type-b1 text-gray-600 max-w-2xl text-center mx-auto mt-12">Hi there. We haven't started posting any jobs yet, but be sure to check again in the future.</p>
+            <p class="type-b1 text-gray-600 max-w-2xl text-center mx-auto mt-12">Hi there. There currently aren't any jobs posted, but be sure to check again soon.</p>
         @endif
         @foreach($posts as $post)
             @include('front.job-posts.index-card', ['post' => $post])
